b836b4d2a351e3abf9aa0472f5bd6085a3dde428
Donatella7 <campaniellodonatella@gmail.com>
2022-03-18 16:38:41 +0100

    click solo sull'ultimo

--- js/myscript.js
+++ js/myscript.js
@@ -1,28 +1,28 @@
 const app = new Vue({
-
+    
     el:"#app",
-
+    
     data: {
-
+        
         listToDo: [
         ],
-
+        
         // * prendo l'input 
         toDoContent:"",
-
+        
         isDone:"",
-
+        
     },
-
+    
     methods: {
-
+        
         /**
-         * function that get the input value and adds it to a property "text:" of a newObject
-         * @param {*} input obtained from an html tag
-         * 
-         */
+        * function that get the input value and adds it to a property "text:" of a newObject
+        * @param {*} input obtained from an html tag
+        * 
+        */
         getInput(input){
-
+            
             newObject = this.creatNewObject();
             if(input != ""){
                 newObject.text = input;
@@ -34,7 +34,7 @@ const app = new Vue({
                 console.log("to-do item empty");
             }
         },
-
+        
         todoClick(){
             for(let i = 0; i<this.listToDo.length - 1;i++){
                 isClicked = true;
@@ -44,19 +44,19 @@ const app = new Vue({
             
         },
         
-
+        
         /*  todoClick(){
             isClicked = true;
             newObject.done = isClicked;  
             return isClicked;      
         }, */
-
-
+        
+        
         /**
-         * Function that removes at the given index and checks if the item at that index exist
-         * @param {*} index given index 
-         */
-
+        * Function that removes at the given index and checks if the item at that index exist
+        * @param {*} index given index 
+        */
+        
         removeInput(index){
             if(this.listToDo[index] != ""){
                 this.listToDo.splice(index,1);
@@ -64,13 +64,13 @@ const app = new Vue({
                 console.warn("you can't remove an element that doesn't exist!!");
             }
         },
-
+        
         creatNewObject(){
             return {
                 text:"",
                 done:false,
             }
         } 
-
+        
     }
 })
